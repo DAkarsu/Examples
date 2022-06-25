c8eeb4fb8d05270e2bdb61f782ed7da20b2fff94
DAkarsu <75308487+DAkarsu@users.noreply.github.com>
2022-06-25 10:54:38 +0300

    Add files via upload

---
+++ MovieRentalSystem/StandardCustomer.java
@@ -0,0 +1,14 @@
+package MovieRentalSystem;
+
+public class StandardCustomer extends Customer{
+
+    public MovieItem movieItem;
+
+    public StandardCustomer(String name, int id) {
+        super(name, id);
+    }
+
+    public void setMovieItem(MovieItem movieItem) {
+        this.movieItem = movieItem;
+    }
+}
