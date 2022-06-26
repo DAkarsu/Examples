69b4852092e53537b6f439cc8a5f4e3b1dde2da6
DAkarsu <75308487+DAkarsu@users.noreply.github.com>
2022-06-26 12:00:05 +0300

    Add files via upload

---
+++ AirlinesManagment/Airport.java
@@ -0,0 +1,11 @@
+package AirlinesManagment;
+
+public class Airport {
+    public int id;
+    public String city;
+
+    public Airport(int id, String city) {
+        this.id = id;
+        this.city = city;
+    }
+}
---
+++ AirlinesManagment/SecondPilot.java
@@ -0,0 +1,8 @@
+package AirlinesManagment;
+
+public class SecondPilot extends Pilot{
+
+    public SecondPilot(int id, String name) {
+        super(id, name);
+    }
+}
